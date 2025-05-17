8c1ce4f1b720b310ad3e03b1f4beda34a5fa68ed
Alex Miller <mlrdev22@gmail.com>
2025-05-17 14:27:02 -0400

    fixed type error on ProjectCard

--- src/app/components/Projectcard.tsx
+++ src/app/components/Projectcard.tsx
@@ -1,9 +1,13 @@
 // Alex Miller
 // Project container for each project
 
+type ProjectCardProps = {
+    title: string,
+    description: string,
+    link: string,
+}
 
-
-const ProjectCard = ({ title, description, link }) => {
+const ProjectCard = ({ title, description, link }: ProjectCardProps) => {
     return (
         <div className="px-10 py-10">
             <h2 className="text-2xl font-semibold border-l-7 border-blue-500 pl-4 mb-4">{title}</h2>
