a8696646dd420668dfc8c6a7d2a04e97abb8b58e
Alex Miller <mlrdev22@gmail.com>
2025-05-17 18:55:57 -0400

    fixed title issue with use client

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,12 +6,9 @@
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Timeline from '../components/Timeline';
-
+import Head from 'next/head';
 import dynamic from "next/dynamic";
 
-export const metadata = {
-    title: 'AM - About'
-}
 
 const Map = dynamic(() => import("../components/Map"), {
   ssr: false,
@@ -22,39 +19,45 @@ export default function AboutPage() {
 
     
     return(
-        <div className="min-h-screen flex flex-col">
 
-            {/* Header at the top */}
-            <Header />
-            
-            {/* main container */}
-            <main className="flex flex-col md:flex-row justify-between px-6 py-10 gap-10 flex-1">
+        <>
+            <Head>
+                <title>AM - About</title>
+            </Head>
+            <div className="min-h-screen flex flex-col">
 
-                {/* map with points */}
-                <section className="md:w-1/3">
-                    <Map />
-                </section>
+                {/* Header at the top */}
+                <Header />
+                
+                {/* main container */}
+                <main className="flex flex-col md:flex-row justify-between px-6 py-10 gap-10 flex-1">
 
-                {/* blurb on me and this page */}
-                <section className="py-6 px-4 text-center">
-                    <div className="max-w-3xl mx-auto">
-                        This shows my Educational / Academic / Personal journey along with some 
-                        information about where I am from. The projects themselves are just
-                        some notable ones I have done and am proud of.
-                    </div>
-                </section>
+                    {/* map with points */}
+                    <section className="md:w-1/3">
+                        <Map />
+                    </section>
+
+                    {/* blurb on me and this page */}
+                    <section className="py-6 px-4 text-center">
+                        <div className="max-w-3xl mx-auto">
+                            This shows my Educational / Academic / Personal journey along with some 
+                            information about where I am from. The projects themselves are just
+                            some notable ones I have done and am proud of.
+                        </div>
+                    </section>
 
 
-                {/* timeline of work and school history */}
-                <section className="md:w-1/3">
-                    <Timeline />
-                </section>
+                    {/* timeline of work and school history */}
+                    <section className="md:w-1/3">
+                        <Timeline />
+                    </section>
 
 
-            </main>
+                </main>
 
-            {/* Footer at the bottom */}
-            <Footer />
-        </div>
+                {/* Footer at the bottom */}
+                <Footer />
+            </div>
+        </>
     );
 }
