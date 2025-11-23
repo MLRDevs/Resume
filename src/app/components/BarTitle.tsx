eb49dac948e60820c7ba589082604a75786e2755
Alex <mlrdev22@gmail.com>
2025-11-23 16:24:25 -0700

    changed up the About Me page with the new styles and mapped the timeline closer to new resume

--- src/app/components/BarTitle.tsx
+++ src/app/components/BarTitle.tsx
@@ -7,7 +7,7 @@ export default function BarTitle({ title }: { title:string }) {
     return (
         <div className="w-full flex flex-col justify-center items-center">
             <div className="p-1 rounded-xl w-3/4 bg-neon-teal/40 m-5"></div>
-            <p className="font-bold text-neon-teal">{title}</p>
+            <p className="font-bold text-neon-teal text-center">{title}</p>
             <div className="p-1 rounded-xl w-3/4 bg-neon-teal/40 m-5"></div>
         </div>
     )
--- src/app/components/Timeline.tsx
+++ src/app/components/Timeline.tsx
@@ -3,91 +3,140 @@
 
 'use client'
 
-import { useState } from "react"; // use states to change whats on the page
-import { FaCode, FaGraduationCap, FaBriefcase, FaProjectDiagram, FaUser } from "react-icons/fa";
-import { MdOutlinePhotoSizeSelectSmall } from "react-icons/md";
+import { useState, useEffect } from "react"; // use states to change whats on the page
+import { FaCode, FaGraduationCap, FaAward, FaDatabase, FaRegLightbulb, FaTools, FaBriefcase, FaProjectDiagram, FaUser } from "react-icons/fa";
+import { IoTerminalOutline } from 'react-icons/io5';
 
 const Timeline = () => {
 
     const [filter, setFilter] = useState('');
 
+    useEffect(() => {
+        setFilter('All');
+    },[]);
+
     const items = [
-        { type: 'Skills', title: 'HTML, CSS, Javascript', date: '2015'},
-        { type: 'Skills', title: 'Java', date: '2018'},
-        { type: 'Personal', title: 'Problem Solving', date: ''},
-        { type: 'Education', title: 'Calumet High School', date: 'Sep 2015 - June 2018'},
-        { type: 'Projects', title: 'Governor\'s Cyber Challenge', date: '2018'},
-        { type: 'Personal', title: 'Adaptability', date: ''},
-        { type: 'Education', title: 'Westwood High School', date: 'Sep 2018 - May 2019'},
-        { type: 'Skills', title: 'C++', date: '2021'},
-        { type: 'Personal', title: 'Efficiency', date: ''},
-        { type: 'Skills', title: 'Unity, C#', date: '2022'},
-        { type: 'Projects', title: 'RNGeo 2D game', date: '2022'},
-        { type: 'Personal', title: 'Teamwork', date:''},
-        { type: 'Work', title: 'Isle Royale Ferry Line', date: 'May 2022 - August 2022'},
-        { type: 'Skills', title: 'Data Structures', date: '2023'},
-        { type: 'Personal', title: 'Communication', date:''},
-        { type: 'Skills', title: 'Python', date: '2023'},
-        { type: 'Projects', title: 'Python automation', date: '2023'},
-        { type: 'Personal', title: 'Organization', date: ''},
-        { type: 'Education', title: 'Associate\'s of Science ~ Northern Michigan University', date: '2023'},
-        { type: 'Skills', title: 'Networking, REST', date: '2024'},
-        { type: 'Skills', title: 'Assembly', date: '2024'},
-        { type: 'Personal', title: 'Solo Work', date: ''},
-        { type: 'Skills', title: 'Patterns, Agile', date: '2024'},
-        { type: 'Skills', title: 'Docker, Node.js', date: '2024'},
-        { type: 'Personal', title: 'Active Listening', date: ''},
-        { type: 'Work', title: 'Isle Royale Ferry Line', date: 'May 2024 - August 2024'},
-        { type: 'Projects', title: 'CTF', date: '2024-2025'},
-        { type: 'Personal', title: 'Patience', date: ''},
-        { type: 'Skills', title: 'Angular.js, React, Next.js', date: '2025'},
-        { type: 'Personal', title: 'Accountability', date: ''},
-        { type: 'Education', title: 'Bachelor\'s of Science - Computer Science / Mathematics ~ Northern Michigan University', date: '2025'},
+        { type: 'Languages', title: 'Java', date: '2018' },
+        { type: 'Languages', title: 'C++', date: '2021' },
+        { type: 'Languages', title: 'C', date: '2024' },
+        { type: 'Languages', title: 'C#', date: '2022' },
+        { type: 'Languages', title: 'Python', date: '2021' },
+        { type: 'Languages', title: 'Smalltalk', date: '2024' },
+        { type: 'Languages', title: 'Assembly', date: '2024' },
+
+        { type: 'Web Development', title: 'HTML', date: '2015' },
+        { type: 'Web Development', title: 'CSS', date: '2015' },
+        { type: 'Web Development', title: 'Javascript', date: '2015' },
+        { type: 'Web Development', title: 'NodeJs', date: '2024' },
+        { type: 'Web Development', title: 'ExpressJs', date: '2024' },
+        { type: 'Web Development', title: 'Typescript', date: '2025' },
+        { type: 'Web Development', title: 'React', date: '2025' },
+        { type: 'Web Development', title: 'NextJs', date: '2025' },
+        { type: 'Web Development', title: 'Angular', date: '2025' },
+        { type: 'Web Development', title: 'TailwindCSS', date: '2025' },
+        { type: 'Web Development', title: 'Vercel', date: '2025' },
+        { type: 'Web Development', title: 'Neon', date: '2025' },
+
+        { type: 'Tools', title: 'Windows', date: '2015' },
+        { type: 'Tools', title: 'Linux', date: '2018' },
+        { type: 'Tools', title: 'Github', date: '2020' },
+        { type: 'Tools', title: 'Unity', date: '2022' },
+        { type: 'Tools', title: 'Docker', date: '2024' },
+        { type: 'Tools', title: 'Android Studio', date: '2024' },
+
+        { type: 'Databases', title: 'SQLite', date: '2023' },
+        { type: 'Databases', title: 'MySQL', date: '2024' },
+        { type: 'Databases', title: 'PostgreSQL', date: '2025' },
+        { type: 'Databases', title: 'Prisma', date: '2025' },
+
+        { type: 'Concepts', title: 'Object-Oriented Programming (OOP)', date: '2019' },
+        { type: 'Concepts', title: 'Data Structures', date: '2022' },
+        { type: 'Concepts', title: 'Networking', date: '2022' },
+        { type: 'Concepts', title: 'Agile Development', date: '2024' },
+        { type: 'Concepts', title: 'Rest APIs', date: '2024' },
+        { type: 'Concepts', title: 'Dynamic Programming', date: '2024' },
+        { type: 'Concepts', title: 'Algorithms', date: '2024' },
+        { type: 'Concepts', title: 'SaaS / PaaS', date: '2025' },
+
+        { type: 'Soft Skills', title: 'Communication', date: '' },
+        { type: 'Soft Skills', title: 'Teamwork', date: '' },
+        { type: 'Soft Skills', title: 'Solo Work', date: '' },
+        { type: 'Soft Skills', title: 'Problem Solving', date: '' },
+        { type: 'Soft Skills', title: 'Adaptability', date: '' },
+        { type: 'Soft Skills', title: 'Efficiency', date: '' },
+        { type: 'Soft Skills', title: 'Organization', date: '' },
+        { type: 'Soft Skills', title: 'Active Listening', date: '' },
+        { type: 'Soft Skills', title: 'Patience', date: '' },
+        { type: 'Soft Skills', title: 'Accountability', date: '' },
+
+        { type: 'Education', title: 'Calumet High School', date: 'Sep 2015 - June 2018' },
+        { type: 'Education', title: 'Westwood High School ~ Diploma', date: 'Sep 2018 - May 2019' },
+        { type: 'Education', title: 'Associate\'s of Science', date: 'Northern Michigan University, Graduated: Dec 2023' },
+        { type: 'Education', title: 'Bachelor\'s of Science ~ Computer Science, Minor in Mathematics', date: 'Northern Michigan University, Graduated: May 2025' },
+
+
+        { type: 'Work', title: 'Isle Royale Ferry Line', date: 'Summers 2022-2025'},
+
+        { type: 'Awards', title: 'Michigan Governor\'s High School Cyber Challenge', date: '2018' },
+
     ];
 
     const iconmap:any = {
-        Skills: <FaCode size={16} />,
-        Education: <FaGraduationCap size={16} />,
-        Projects: <FaProjectDiagram size={16} />,
-        Work: <FaBriefcase size={16} />,
-        Personal: <FaUser size={16} />
+        Languages: <IoTerminalOutline size={24} />,
+        'Web Development': <FaCode size={24} />,
+        Tools: <FaTools size={24} />,
+        Databases: <FaDatabase size={24} />,
+        Concepts: <FaRegLightbulb size={24} />,
+        Education: <FaGraduationCap size={24} />,
+        Work: <FaBriefcase size={24} />,
+        'Soft Skills': <FaUser size={24} />,
+        Awards: <FaAward size={24} />
     }
 
-    const filteritems = filter === 'all'
+    const filteritems = filter === 'All'
         ? items
         : items.filter(item => item.type === filter);
 
+    const FilterTypes:string[] = [
+        'Languages', 
+        'Web Development', 
+        'Tools', 
+        'Databases', 
+        'Concepts', 
+        'Soft Skills', 
+        'Education', 
+        'Work', 
+        'Awards',
+    ]
+
     return (
-        <div className="">
+        <div className="w-110 md:w-150 2xl:w-full flex flex-col justify-center items-center gap-5">
 
             {/* dropdown menu */}
             <div className="mb-6">
-                <label htmlFor="filter" className="mr-2 font-bold text-xl">Filter:</label>
                 <select
                     value={filter}
                     onChange={(e) => setFilter(e.target.value)}
-                    className="border px-2 py-1 rounded-xl text-black bg-white"
+                    className="border px-2 py-1 rounded-xl text-ice-white font-bold bg-neon-teal/40"
                 >
-                    <option value="all">All</option>
-                    <option value="Skills">Tech Skills</option>
-                    <option value="Projects">Projects</option>
-                    <option value="Work">Work</option>
-                    <option value="Education">Education</option>
-                    <option value="Personal">Soft Skills</option>
+                    <option className="bg-navy-blue font-bold" value="All">All</option>
+                    {FilterTypes.map((t:any) => (
+                        <option key={t} className="bg-navy-blue font-bold" value={t} >{t}</option>
+                    ))}
                 </select>
 
             </div>
 
 
             {/* Timeline */}
-            <ul className="max-h-75 overflow-y-auto w-80 overflow-x-hidden">
+            <ul className="h-75 m-5 overflow-y-auto w-100 md:w-150 2xl:w-full overflow-x-hidden">
                 {filteritems.map((item,index) => (
-                    <li key={index} className="mb-10 ml-2 relative w-75 overflow-x-hidden">
+                    <li key={index} className="mt-5 relative w-full overflow-x-auto">
                         <div className="flex items-center gap-3">
                             <span className="text-xl">{iconmap[item.type]}</span>
                             <p className=" font-bold capitalize">{item.title}</p>
                         </div>
-                        <p className=" text-sm text-gray-500">{item.date}</p>
+                        <p className="text-sm text-gray-500">{item.date}</p>
                     </li>
                 ))}
             </ul>
